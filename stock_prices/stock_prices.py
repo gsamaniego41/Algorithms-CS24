cfffe1bb549f16d9893384a200452ffc19eb066d
Gabriel Samaniego <selfdev.gs@gmail.com>
2019-12-11 18:48:50 -0800

    recipe_batches first pass

--- stock_prices/stock_prices.py
+++ stock_prices/stock_prices.py
@@ -2,51 +2,13 @@
 
 import argparse
 
-# init commit
 
-# prices = [1050, 270, 1540, 3800, 2]
-# 1050 270 1540 3800 2
-
-
-# def find_max_profit(prices):
-#     highest_price = max(prices)
-#     new_list = prices[0:prices.index(highest_price)]
-#     print(new_list)
-#     # lowest_before = min(new_list, default=0)
-#     # print(new_list)
-#     lowest = highest_price
-
-#     for i in new_list:
-#         if i < lowest:
-#             lowest = new_list[i]
-
-#     # lowest_before = min(prices[:prices.index(highest_price)])
-#     # print('find by index', prices.index(lowest_before))
-#     # print('min', lowest_before)
-#     print('min', lowest)
-
-#     # find highest prices in list
-#     # for i in prices:
-#     #     print(i)
-
-#     # for j in range(len(prices), -1, -1):
-
-#     print('max', highest_price)
-
-#     # return highest_price - lowest_before
-#     return highest_price - lowest
-
-# 100 90 80 50 20 10
 def find_max_profit(prices):
     result = 0
     print(prices)
     highest_price = max(prices[1:])
     new_list = prices[0:prices.index(highest_price) + 1]
-    print('new list', new_list)
     lowest_before = min(new_list)
-    print('min', lowest_before)
-    print('max', highest_price)
-    print('result', highest_price - lowest_before)
 
     if len(new_list) < 3:
         result = new_list[1] - new_list[0]
